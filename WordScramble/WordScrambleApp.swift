f68ec8ef0d725a3548a9f2f5b7bfb819a264df60
ysmaraujo <yasminnaraujo@hotmail.com>
2024-06-13 15:36:45 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yasmin araujo on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
